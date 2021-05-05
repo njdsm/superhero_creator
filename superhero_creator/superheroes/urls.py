e7910382aa36ddea0fcf6f7a2284a019a6482b91
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-05 11:35:27 -0500

    completed tutorial, began work on getting edit functionality.

--- superhero_creator/superheroes/urls.py
+++ superhero_creator/superheroes/urls.py
@@ -5,4 +5,6 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:superhero_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('edit/', views.edit, name='edit')
 ]
--- superhero_creator/superheroes/views.py
+++ superhero_creator/superheroes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Superhero
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -17,3 +19,28 @@ def detail(request, superhero_id):
         'specific_superhero': specific_superhero
     }
     return render(request, 'superheroes/detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        superhero_name = request.POST.get('superhero_name')
+        alter_ego_name = request.POST.get('alter_ego_name')
+        primary_ability = request.POST.get('primary_ability')
+        alternate_ability = request.POST.get('alternate_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(superhero_name=superhero_name, alter_ego_name=alter_ego_name,
+                                  primary_ability=primary_ability, alternate_ability=alternate_ability,
+                                  catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+
+def edit(request, superhero_id):
+    superhero = Superhero.objects.get(id=superhero_id)
+    context = {
+        'superhero': superhero
+    }
+    return render(request, 'superheroes/edit.html', context)
+
